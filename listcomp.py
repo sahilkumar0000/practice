bbb95f2fa7c7c2710a9ee56cab5be4c8ad150e64
sahilkumar0000 <9899982913aa@gmail.com>
2022-08-28 00:53:32 +0530

    28-8-2022

---
+++ listcomp.py
@@ -0,0 +1,23 @@
+x = int(input())
+y = int(input())
+z = int(input())
+n = int(input())
+a=[]
+i=0
+j=0
+k=0
+a.append(i)
+a.append(j)
+a.append(k)
+while i<x:
+    i+=1
+    print(a)
+    while j<y:
+        a.append(y)
+        print(a)
+        while k<z:
+                a.append(z)
+                print(a)
+                    
+                k+=1
+                    
---
+++ loops.py
@@ -0,0 +1,5 @@
+n=3
+i=0
+while i<=n:
+    print(i*i)
+    i+=1
