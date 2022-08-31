18c86c28e3fe6a039a2af07821e009752fa65dda
sahilkumar0000 <9899982913aa@gmail.com>
2022-08-31 19:17:33 +0530

    31-8-2022-2

--- practice6.py
+++ practice6.py
@@ -42,6 +42,69 @@
 #     print("you are pass")
 
 # Question 3
-spam=["make a lot of money", "buy now", "subscribe this", "click this"]
-comment=input("Enter your Comment: ")
 
+# comment=input("Enter your Comment: ")
+# if "make a lot of money" in comment:
+#     spam=True
+# elif "buy now"in comment:
+#     spam= True
+# elif "subscribe this" in comment:
+#     spam =True
+# elif "click this" in comment:
+#     spam=True                                    1st method
+# else:
+#     spam=False
+
+# if spam:
+#     print("Your text is Spam")
+# else:
+#     print("Thanks For Your Valuable Comment")
+
+
+# spam=["click this","subscribe this","buy now","make a lot of money"]
+# comment=input("Enter your comment: ")
+# if comment in spam:
+#     print("Your Comment is Spam")
+# else:
+#     print("Thanks for your comment")
+
+# Question 4
+# a=input("Enter your Name: ")
+# if len(a)>10:
+#     print("Your Name contains more then 10 Characters")
+# else:
+#     print("Great Name sir!!")
+
+# Question 5
+# a=["sahil", "shubham","sheetal","sumit","mohit"]
+# name=input("Enter your Name:")
+# if name in a:
+#     print("Your Name is already in the List")
+# else:
+#     print("Your Name is not present in the List")
+
+# Question 6
+# marks=int(input("Enter your Marks: "))
+# if marks>100 or marks<0:
+#     print("Invalid Number")
+# elif marks<100 and marks>=90:
+#     print("Ex")
+# elif marks<90 and marks>=80:
+#     print("A")
+# elif marks<80 and marks>=70:
+#     print("B")
+# elif marks<70 and marks>=60:
+#     print("C")
+# elif marks<60 and marks>=50:
+#     print("D")
+# else:
+#     print("F")
+
+
+# Question 7
+post=input("Enter your post: ")
+a="harry"
+if a in post:
+    print("Post is talking about harry")
+else:
+    print("post is not talking about harry")
