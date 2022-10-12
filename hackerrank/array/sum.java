3097b2f8c5c2716a3e87475780fbb997f1fe77f9
sahilkumar0000 <9899982913aa@gmail.com>
2022-10-12 23:58:46 +0530

    back

---
+++ hackerrank/array/sum.java
@@ -0,0 +1,10 @@
+class sum{
+    public static void main(String[]args){
+        int arr[]={12,13,14,15,16};
+        int sum=0;
+        for(i=0;i<arr.length;i++){
+            sum=sum+arr[i]
+        }
+        System.out.println(sum);
+    }
+}
